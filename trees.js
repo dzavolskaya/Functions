7109280f7057ec58b77d46279302b8bd82688c7e
dzavolskaya <darya.zavolskaya@gmail.com>
2019-06-28 22:16:17 +0200

    added functions in trees.js

--- trees.js
+++ trees.js
@@ -4,12 +4,12 @@ const tree = {
         children: [
           {
             children: [],
-            meta: { size: 4000 },
+            meta: {},
             name: 'NgiNx',
             type: 'directory',
           },
           {
-            children: [{ meta: { uid: 0 }, name: 'config.JSON', type: 'file' }],
+            children: [{ meta: {  size: 1200 }, name: 'config.JSON', type: 'file' }],
             meta: {},
             name: 'CONSUL',
             type: 'directory',
@@ -19,12 +19,14 @@ const tree = {
         name: 'eTc',
         type: 'directory',
       },
-      { meta: {}, name: 'cohOsts', type: 'file' },
+      { meta: { size: 8200 }, name: 'cohOsts', type: 'file' },
+      { meta: { size: 400 }, name: 'resolve', type: 'file' },
     ],
     meta: {},
     name: '/',
     type: 'directory',
   };
+
 
 const downcaseFileNames = (tree) => {
   if (tree.type === 'file') {
@@ -36,6 +38,7 @@ const downcaseFileNames = (tree) => {
   return { ...tree, children: tree.children.map(downcaseFileNames)};
 };
 
+//map function for trees
 const mapTransformer = (f, tree) => {
   const newResult = f(tree);
   if (!tree.children) {
@@ -45,6 +48,7 @@ const mapTransformer = (f, tree) => {
 };
 // console.log(mapTransformer(n => ({ ...n, name: n.name.toUpperCase() }), tree));
 
+//filter function for trees
 const filterTransformer = (f, tree) => {
   if (!f(tree)) return null;
   if (!tree.children) {
@@ -54,7 +58,7 @@ const filterTransformer = (f, tree) => {
 };
 // console.log(filterTransformer(n => n.type === 'directory', tree));
 
-
+//reduce function to count number of files in the tree
 const reduceTransformer = (f, tree, acc) => {
   const newAcc = f(acc,tree);
   if (tree.type === 'file') {
@@ -77,3 +81,65 @@ const findFilesByName = (tree, str) => {
   return iter(tree, '', []);
 };
 //console.log(findFilesByName(tree, 'co'));
+
+// ====================================================//
+//function reduce for trees
+const reduce = (f, tree, acc) => {
+  const newAcc = f(acc,tree);
+  if (!tree.children) {
+    return newAcc;
+  }
+  return tree.children.reduce((iAcc, n) => reduceTransformer(f, n, iAcc), newAcc);
+};
+
+//function to simulate Linux command 'du'
+const du = (tree) => {
+  const countFilesSize = node => reduce(
+    (acc, n) => (n.type === 'file' ? acc + n.meta.size : acc), node,
+    0,
+  );
+  if (!tree.children) {
+    if (tree.type === 'file') {
+      return [tree.name, tree.meta.size];
+    }
+  }
+  const result = reduce((acc, element) => {
+    if (element.type === 'directory' && tree.children.length !== 0) {
+      return tree.children
+        .map(el => [el.name, countFilesSize(el)]);
+    }
+    return acc;
+  }, tree, []);
+  return result.sort((a, b) => (b[1] - a[1]));
+};
+//console.log(du(tree));
+// ====================================================//
+
+const list = [1, 2, [3, 5], [[4, 3], 2]];
+
+const flatten = list => list
+  .reduce((acc, element) => acc.concat(Array.isArray(element) ? flatten(element) : element), []);
+
+//OR
+const flatten2 = list => list.reduce((acc, element) => {
+  const result = (Array.isArray(element) ? [...acc, ...flatten(element)] : [...acc, element]);
+  return result;
+}, []);
+
+//==================================================//
+//function to convert nested array into nested object
+const nestedArray = [
+  ['key', [['key3', 'anotherValue']]],
+  ['key2', 'value2']
+];
+const convertArray = array => array
+  .reduce((acc, item) => {
+    const [key, value] = item;
+    return { ...acc, [key]: Array.isArray(value) ? convertArray(value) : value };
+  }, {});
+//OR
+const convertArray2 = array => array.reduce(
+  (acc, [key, value]) => ({ ...acc, [key]: value instanceof Array ? convertArray2(value) : value }),
+  {},
+);
+//==================================================//
